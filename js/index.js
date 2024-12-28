6629e50025ed1cea67f27b4dddfb779d4ae19821
MooAttia <mohamedattia101430@gmail.com>
2024-12-28 13:37:54 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function() {
+    var nav = document.querySelector('nav');
+    var header = document.querySelector('header');
+    
+    if (window.scrollY > header.offsetHeight) {
+      nav.classList.remove('navbar-hidden');
+      nav.classList.add('navbar-visible');
+    } else {
+      nav.classList.remove('navbar-visible');
+      nav.classList.add('navbar-hidden');
+    }
+  });
