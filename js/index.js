c4e37ccb9426f5e39a78ea3b6265ba64c3ac3c1e
MooAttia <mohamedattia101430@gmail.com>
2024-12-29 00:37:56 +0200

    3

--- js/index.js
+++ js/index.js
@@ -13,26 +13,6 @@ window.addEventListener('scroll', function() {
 
 
 
-function hello(x, y) {
-  let x1 = x;
-  let x2 = y;
-
-  return {
-    add: function() {
-      return this.x+this.y
-    },
-    sub: function() {
-      return this.x*this.y
-    },
-    inc: function(num) {
-      if(attr == 'x')this.x++
-      else y++
-    }
-  };
-}
-
-let incrementer = hello(2, 2);
-
 
 
  
