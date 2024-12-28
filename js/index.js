bdfabee5638b9b7d8c8ae5d3f1939690d45522f2
MooAttia <mohamedattia101430@gmail.com>
2024-12-29 01:45:20 +0200

    4th

--- js/index.js
+++ js/index.js
@@ -13,6 +13,40 @@ window.addEventListener('scroll', function() {
 
 
 
+  document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.querySelector("nav");
+    const scrollUser = 250;
+  
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > scrollUser) {
+        navbar.classList.add("fixed-navbar");
+      } else {
+        navbar.classList.remove("fixed-navbar");
+      }
+    });
+  });
+  
 
 
- 
+  document.addEventListener("DOMContentLoaded", () => {
+    const scrollToTopButton = document.getElementById("scrollToTopButton");
+  
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 200) {
+        scrollToTopButton.classList.remove("d-none");
+      } else {
+        scrollToTopButton.classList.add("d-none");
+      }
+    });
+  
+    scrollToTopButton.addEventListener("click", (event) => {
+      event.preventDefault(); 
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    });
+    
+  });
+  let mainOffset = $(".mainSection").offset().top;
+
