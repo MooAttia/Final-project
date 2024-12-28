1dea0ad880ac867842892179631a07ad3add5155
MooAttia <mohamedattia101430@gmail.com>
2024-12-28 23:57:13 +0200

    second

--- js/index.js
+++ js/index.js
@@ -10,3 +10,29 @@ window.addEventListener('scroll', function() {
       nav.classList.add('navbar-hidden');
     }
   });
+
+
+
+function hello(x, y) {
+  let x1 = x;
+  let x2 = y;
+
+  return {
+    add: function() {
+      return this.x+this.y
+    },
+    sub: function() {
+      return this.x*this.y
+    },
+    inc: function(num) {
+      if(attr == 'x')this.x++
+      else y++
+    }
+  };
+}
+
+let incrementer = hello(2, 2);
+
+
+
+ 
